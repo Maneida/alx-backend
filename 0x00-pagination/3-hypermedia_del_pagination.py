148b18ff258a5fd6f95ff2ddc55ee67c63c0de58
Michael Amuah <mkdamuah@hotmail.com>
2023-10-22 10:33:30 +0000

    task 3 commit

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,4 +1,79 @@
 #!/usr/bin/env python3
 """
-Module for
+Deletion-resilient hypermedia pagination
 """
+
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """
+        Returns: {
+            'index': the current start index of the return page
+            'next_index': the next index to query with
+            'page_size': the current page size
+            'data': the actual page of the dataset
+        }
+        """
+        dataset = self.indexed_dataset()
+        
+        start_index = index if index is not None else 0
+        end_index = start_index + page_size
+
+        assert start_index >= 0
+        assert end_index <= len(dataset)
+
+        next_index = (end_index if end_index < len(dataset)
+                        else len(dataset))
+        # next = min(end_index, dataset)
+
+        if self._has_missing_index(dataset):
+            dataset = self._reindex_dataset(dataset)
+
+        data = [dataset[i] for i in range(start_index, next_index)]
+
+        return {"index": index, "next_index": next_index,
+                "page_size": page_size, "data": data}
+
+    def _has_missing_index(self, dataset):
+        indices = sorted(dataset.keys())
+        return any(index != expected_index for expected_index, index in enumerate(indices))
+
+    def _reindex_dataset(self, dataset):
+        new_dataset = {}
+        for i, key in enumerate(sorted(dataset.keys())):
+            new_dataset[i] = dataset[key]
+        return new_dataset
