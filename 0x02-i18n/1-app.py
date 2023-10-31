6222e7db3ffc7fe2c1ad361afda1af2b7f60840c
Michael Amuah <mkdamuah@hotmail.com>
2023-10-31 21:58:47 +0000

    1 corr

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -14,12 +14,8 @@ class Config():
 
 app.config.from_object(Config)
 
-@babel.localeselector
-def get_locale():
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-
-@app.route('/')
+@app.route('/', methods=['GET'], strict_slashes=False)
 def index() -> Response:
     """route to index.html"""
     return render_template('1-index.html')
