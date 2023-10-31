8ecb4a6f4421ba17b323972ee0b89039c196835d
Michael Amuah <mkdamuah@hotmail.com>
2023-10-31 22:07:16 +0000

    1 corr3

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -2,13 +2,15 @@
 """
 Flask app routing
 """
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from flask_babel import Babel
+
 app = Flask(__name__)
 babel = Babel(app)
 
 
 class Config:
+    """ Config Class """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
@@ -19,7 +21,7 @@ app.config.from_object(Config)
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
-    """route to index.html"""
+    """route for index.html"""
     return render_template('1-index.html')
 
 
