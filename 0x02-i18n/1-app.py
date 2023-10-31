1efb0942b2459cc343aac5af6b9d4974bcc7479a
Michael Amuah <mkdamuah@hotmail.com>
2023-10-31 21:52:02 +0000

    1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Flask app routing
+"""
+from flask import Flask, render_template, Response
+from flask_babel import Babel
+app = Flask(__name__)
+babel = Babel(app)
+
+class Config():
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app.config.from_object(Config)
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> Response:
+    """route to index.html"""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
